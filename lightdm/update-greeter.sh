91073a107454303783e02b3a202126289cbc2500
null <jtzk@underground.barracuda>
2024-06-28 23:05:02 -0400

    Update

--- lightdm/update-greeter.sh
+++ lightdm/update-greeter.sh
@@ -10,4 +10,4 @@ sudo cp $DIR_BASE/lightdm/* $DIR_LIGHTDM
 
 sudo cp $HOME/.local/share/lightdm/default.jpg /usr/share/images/lightdm
 sudo cp $HOME/.local/share/lightdm/default.svg /usr/share/images/lightdm
-sudo cp -r lightdm.conf /etc/lightdm/lightdm-gtk-greeter.conf  
+sudo cp -r $DIR_LIGHTDM/lightdm.conf /etc/lightdm/lightdm-gtk-greeter.conf  
